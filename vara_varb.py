07dcbffabc95542fe4bfedcd3d8d1cbb39ece646
Jose Apablaza <japablaza@gmail.com>
2018-09-14 15:38:00 -0500

    Changing the flow for str vars

--- vara_varb.py
+++ vara_varb.py
@@ -1,9 +1,16 @@
-varA = 7
-varB = 8
+varA = 2
+varB = 6
 
 # Moving integer to string
 if ((type(varA) == str) or (type(varB) == str)):
     print('string involved')
+    if (type(varA) == str) and (type(varB) == str):
+        if varA > varB:
+            print("bigger")
+        elif varA == varB:
+            print("equal")
+        else:
+            print("smaller")
     # if type(varA) != str:
     #     varA = str(varA)
     #     if varA > varB:
@@ -20,16 +27,6 @@ if ((type(varA) == str) or (type(varB) == str)):
     #         print("equal")
     #     elif varA < varB:
     #         print("smaller3")
-
-# Testing if varA and varB are string
-# if (isinstance(varA, str) and isinstance(varB, str)):
-if (type(varA) == str) and (type(varB) == str):
-    if varA > varB:
-        print("bigger")
-    elif varA == varB:
-        print("equal")
-    else:
-        print("smaller")
 
 # Testing if varA and varB are integer
 # if (isinstance(varA, int) and isinstance(varB, int)):
